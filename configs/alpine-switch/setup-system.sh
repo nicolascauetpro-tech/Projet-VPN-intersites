a1b99d05da0399224d96776f425b51d238bad23e
nicolascauetpro-tech <nicolas.cauetpro@gmail.com>
2026-01-09 11:02:50 +0100

    Update setup-system.sh

--- configs/alpine-switch/setup-system.sh
+++ configs/alpine-switch/setup-system.sh
@@ -1,1 +1,44 @@
+#!/bin/sh
+
+#================================================================================
+# Nom du script : setup-system.sh
+# Rôle : Automatiser la  sécurisation  et  l'installation d'OVS/FRR
+# Auteur : Nicolas Cauet
+# Projet : VPN  Intersites
+
+echo "--- Début de la configuration  système ---"
+
+# 1. Vérifications de la présence des dépôts Main et Community
+
+cat /etc/apk/repositories
+
+# 2. Vérifications du fonctionnement internet sur  eth0
+ip a
+ping 8.8.8.8
+dig google.com
+
+# 3.  Mise  à jour des paquets
+
+apk update
+
+# 4.Création d'un utilisateur pour l'administration
+
+adduser admin_reseau
+
+# 4. Ajout de l'utilisateur créer pour l'administration dans le groupe wheel
+
+addgroup admin_reseau wheel
+
+# 5. Création du fichier doas.conf pour permettre l'exécution des commandes par l'utilisateur administrateur.
+
+vi /etc/doas.d/doas.conf
+
+## Réstriction des accès au fichier
+
+chmod 600 /etc/doas.d/doas.conf
+
+## Ajout de la ligne suivante
+
+permit :wheel
+
 
