45bfdfce504e10dedde6862ae0795038ebead651
nicolascauetpro-tech <nicolas.cauetpro@gmail.com>
2026-01-09 11:22:15 +0100

    Update setup-system.sh

--- configs/alpine-switch/setup-system.sh
+++ configs/alpine-switch/setup-system.sh
@@ -21,17 +21,23 @@ dig google.com
 
 apk update
 
-# 4.Création d'un utilisateur pour l'administration
+# 4. Création du groupe  administration avec des droits administrations
 
-adduser -D admin_reseau
+addgroup -S admins_reseau
 
-# 4. Ajout de l'utilisateur créer pour l'administration dans le groupe wheel
+# 4.Création de l'utilisateur pour l'administration sans mot  de passe
 
-addgroup admin_reseau wheel
+adduser -D nicolas_admin
 
-# 5. Création du fichier doas.conf pour permettre l'exécution des commandes par l'utilisateur administrateur.
+# 4. Ajout de l'utilisateur créé pour l'administration dans le groupe ===== admins_reseau =====
 
-mkdir -p /etc/doas.d/doas.conf
+addgroup nicolas_admin  admins_reseau
+
+# 5. 
+
+# 5. Création du dossier doas.d et du fichier doas.conf pour permettre l'exécution des commandes par l'utilisateur administrateur.
+
+mkdir -p /etc/doas.d
 
 ## Propriétaire du fichier 
 
@@ -43,6 +49,6 @@ chmod 600 /etc/doas.d/doas.conf
 
 ## Ajout de la ligne suivante
 
-echo "permit :wheel" > /etc/doas.d/doas.conf
+echo "permit :admins_reseau" > /etc/doas.d/doas.conf
 
 
